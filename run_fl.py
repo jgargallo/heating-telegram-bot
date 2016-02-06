5c6f187a744e565e96aceeb94c6cd40142ee0ce3
Jose Gargallo <jgargallo@gmail.com>
2016-02-06 10:57:45 +0100

    update_id control

--- run_fl.py
+++ run_fl.py
@@ -7,17 +7,21 @@ from heating_bot import build_bot
 
 app = Flask(__name__)
 bot = None
+last_update_id = 0
 
 @app.route("/heatingbot/{0}".format(settings.TG_API_KEY), methods=['GET', 'POST'])
 def post():
     body = request.json
-    message = telegram.Update.de_json(body).message
-    if message and message.text is not None:
+    update = telegram.Update.de_json(body)
+    message = update.message
+    update_id = int(update.update_id)
+    if update_id > last_update_id and message and message.text is not None:
         cmd = message.text.lower()
         if cmd.startswith('/on'):
             bot.turnon_cmd(message)
         elif cmd.startswith('/off'):
             bot.turnoff_cmd(message)
+        last_update_id = update_id
     return json.dumps(body)
 
 if __name__ == "__main__":
